52114581590d841b3e76c30b1cf331b34bd206d3
Kattis <kattis.a@gmail.com>
2018-09-30 23:02:23 +0200

    Added hero property

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -7,9 +7,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeroesComponent implements OnInit {
 
+  hero = "Windsurf";
+
   constructor() { }
 
-  ngOnInit() {
-  }
+  ngOnInit() { }
 
 }
